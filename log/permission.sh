788e850807543baf6b9ebc5955cfcfde6ff9584c
mcrai-dev <laydamcrai@gmail.com>
2024-06-04 22:22:17 +0300

    give permission for rkhunter.log to be readable from the script

---
+++ log/permission.sh
@@ -0,0 +1,3 @@
+chmod 644 /home/mcrai/Desktop/ENI/CyberSec/antispyware/autmation/log/chkrootkit.log
+chmod 644 /home/mcrai/Desktop/ENI/CyberSec/antispyware/autmation/log/lynis.log
+chmod 644 /home/mcrai/Desktop/ENI/CyberSec/antispyware/autmation/log/rkhunter.log
